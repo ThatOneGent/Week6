34ce3f2bb3b5bc38d962057d140258012e7e7d3c
Markus <119375162+ThatOneGent@users.noreply.github.com>
2023-01-28 12:52:40 -0500

    clean up of comment/code Final push

--- week6_unitTest.js
+++ week6_unitTest.js
@@ -3,50 +3,40 @@ var expect = chai.expect;
 describe('myFunctions', function(){
     describe('#shuffleDeck', function(){
 
-/*         it('should concatenate the two parameters', function(){
-            var x = doSomething('hello',5);
-            expect(x).to.equal('hello5');
-        }); */
 
         it('should shuffle array contents',function(){
-
+            // Test array created to mimic deck
             testArray= {deckContents: [1,2,3,4,5,'a','b','c']};
+            // deck created based off class
             let myClass = new Deck();
+            //making them match
+            myClass.deckContents=testArray.deckContents;
+            //shuffling created deck
             let myMethod = myClass.shuffleDeck(testArray.deckContents);  
-            
-            //let shufArray = 0;
+            //running expected unit test
             expect(testArray).to.not.equal(myMethod);
 
-              //  .then(function () {
-              //   console.log("Assertion Passed");
-                    //});
 
-            //console.log(myMethod);
         }); 
 
-/* 
-        it('should throw an error if first param is not a string',function(){
-            expect(function(){
-                doSomething(5,5);
-            }).to.throw(Error);
-        }) */
     });
 
 
     describe('#createDeck',function(){
+            //Test deck based off deck class method
+            // this will test both of the next two 'it' functions
             const deckTest = new Deck();
 
         it('should create a deck from deck class', function(){
-            //const deckTest = new Deck();
+            //create a deck of Cards within the Deck
             deckTest.createDeck();
+            //did it actually create a deck of deck class??
             expect(deckTest).to.be.instanceOf(Deck);
-            //expect(deckTest.deckContents.length).to.equal(52);
 
         });
         it('should contain 52 cards', function(){
-
+            //did the createDeck method create 52 cards?
             expect(deckTest.deckContents.length).to.equal(52);
-
         });
 
     });
