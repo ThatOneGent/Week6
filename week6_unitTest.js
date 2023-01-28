eba2c09bbdf3eb2a46092531eee82d8c965d33d5
Markus <119375162+ThatOneGent@users.noreply.github.com>
2023-01-28 15:59:14 -0500

    false positive issue corrected

--- week6_unitTest.js
+++ week6_unitTest.js
@@ -5,32 +5,22 @@ describe('myFunctions', function(){
 
 
         it('should shuffle array contents',function(){
-            // Test array created to mimic deck
+
+            // Create two test decks
             let testDeck1 = new Deck();
-            // deck created based off class
             let testDeck2 = new Deck();
 
+            //populate both decks with cards
             testDeck1.createDeck();
             testDeck2.createDeck();
 
+            //Shuffle only the second deck
             testDeck2.deckContents = testDeck2.shuffleDeck(testDeck2.deckContents);
-        
 
-
-            //shuffling created deckContents
-            //myClass.deckContents = myClass.shuffleDeck(myClass.deckContents);  
-
-
-           // let myMethod = myClass.shuffleDeck(myClass.deckContents);  
-            //running expected unit test
-
+            //contents should no longer match.
             expect(testDeck1.deckContents).to.not.equal(testDeck2.deckContents);
             console.log(testDeck1);
             console.log(testDeck2);
-            //expect(testArray.deckContents).to.not.equal(myClass.deckContents);
-
-
-
 
         }); 
 
